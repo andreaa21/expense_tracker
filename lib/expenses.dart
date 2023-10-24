8297447238fe91eb185ee07843c00fd6119e8204
Andrea Zollini <andrea.zollini@dgalab.it>
2023-10-24 18:00:02 +0200

    boh

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -12,6 +12,13 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text('chart'),
+          Text('Expenses'),
+        ],
+      ),
+    );
   }
 }
